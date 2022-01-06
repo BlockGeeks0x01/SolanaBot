b952d66e816e5573ba6b09263d7fdd6666e5b9b7
EricSun <ericsgy@163.com>
2022-01-06 16:05:52 +0800

    add remark

--- check_balance.py
+++ check_balance.py
@@ -8,13 +8,12 @@ from utils import send_telegram_notice
 client = Client('https://api.mainnet-beta.solana.com')
 
 check_list = (
-    (TokenAccount.CWAR, "CWAR"),
-    (TokenAccount.BLOCK, "BLOCK"),
-    (TokenAccount.UNQ, "UNQ"),
-    (TokenAccount.MEAN, "MEAN"),
-    (TokenAccount.GWT, "GWT"),
-    (TokenAccount.SCY, "SCY"),
-    (TokenAccount.SLC, "SLC"),
+    (TokenAccount.CWAR, "CWAR", ""),
+    (TokenAccount.BLOCK, "BLOCK", "cost: 0.078"),
+    (TokenAccount.UNQ, "UNQ", ""),
+    (TokenAccount.MEAN, "MEAN", ""),
+    (TokenAccount.GWT, "GWT", ""),
+    (TokenAccount.SLC, "SLC", "cost: 0.07"),
 )
 
 
@@ -29,13 +28,13 @@ if __name__ == '__main__':
 
     while True:
         send_flag = False
-        for addr, token in check_list:
+        for addr, token, remark in check_list:
             rst = client.get_token_account_balance(addr)
             balance = int(rst['result']['value']['amount'])
             balance_of_readable = rst['result']['value']['uiAmountString']
             logging.info(f"{token} balance: {balance_of_readable}")
             if balance > 0:
-                msg = f"{token} balance: {balance_of_readable}"
+                msg = f"{token} balance: {balance_of_readable} [{remark}]"
                 send_telegram_notice(msg)
                 send_flag = True
 
